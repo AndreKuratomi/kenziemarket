92b4d7557b067223dbf758cda0e40e8fa86d939b
André Kuratomi <andrekuratomi@gmail.com>
2022-02-08 17:31:34 -0300

    feat: product table drafted

---
+++ src/entities/Product.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("products")
+export class Product {
+  @PrimaryGeneratedColumn()
+  id!: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  price: number;
+
+  @CreateDateColumn()
+  createdOn!: Date;
+
+  constructor(name: string, type: string, price: number) {
+    this.name = name;
+    this.type = type;
+    this.price = price;
+  }
+}
