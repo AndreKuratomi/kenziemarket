bd4dbdaf174fff16662b5d35df4f479635820a55
André Kuratomi <andrekuratomi@gmail.com>
2022-02-09 11:57:34 -0300

    product  controller skelleton made

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+export const registerProduct = async (req: Request, res: Response) => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
+
+export const listAllProducts = async (req: Request, res: Response) => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
+
+export const listOneProduct = async (req: Request, res: Response) => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
