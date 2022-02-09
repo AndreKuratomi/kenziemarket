9efd5eef52bef1fe9b3926237530e1e0033eedb7
André Kuratomi <andrekuratomi@gmail.com>
2022-02-09 12:28:50 -0300

    cart controller skelleton updated

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -4,15 +4,37 @@ import { getRepository } from "typeorm";
 import { Product } from "../entities/Product";
 import { User } from "../entities/User";
 
-export const addToCart = async () => {
-  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+export const createCart = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
 };
-export const addToCart = async () => {
-  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+
+export const addToCart = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
 };
-export const addToCart = async () => {
-  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+
+export const listAllCarts = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
 };
-export const addToCart = async () => {
-  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+
+export const listOneCart = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
+};
+
+export const deleteCart = async (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {
+    res.status(error.statusCode).json({ message: error.message });
+  }
 };
