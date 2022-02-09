c77cbc639661440b4983c67ac7a6f137f651feeb
André Kuratomi <andrekuratomi@gmail.com>
2022-02-09 12:23:23 -0300

    cart controller skelleton made

---
+++ src/controllers/cart.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import { Product } from "../entities/Product";
+import { User } from "../entities/User";
+
+export const addToCart = async () => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
+export const addToCart = async () => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
+export const addToCart = async () => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
+export const addToCart = async () => {
+  // try {} catch (error: any) {res.status(error.statusCode).json({message: error.message})}
+};
