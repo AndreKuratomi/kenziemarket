196ed6350b1d1b5f04185dc56f66f0c5908d40d1
André Kuratomi <andrekuratomi@gmail.com>
2022-02-11 17:52:14 -0300

    feat: Entities updated with real relations

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -33,22 +33,13 @@ class User {
   @OneToOne((type) => Cart, (cart) => cart.user)
   cart!: Cart;
 
-  // @OneToMany((type) => Product, (product) => product.user) products!: Product[];
-
   @OneToMany((type) => Sell, (sell) => sell.user)
   sells!: Sell[];
 
-  constructor(
-    name: string,
-    email: string,
-    password: string,
-    // cart: object[],
-    isAdm: boolean
-  ) {
+  constructor(name: string, email: string, password: string, isAdm: boolean) {
     this.name = name;
     this.email = email;
     this.password = password;
-    // this.cart = cart;
     this.isAdm = isAdm;
   }
 }
