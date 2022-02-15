92a7e2acefd44528340f91525b90c86f19e71aae
André Kuratomi <andrekuratomi@gmail.com>
2022-02-14 21:22:21 -0300

    fix: Sells connection trouble solved

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,11 @@ import { Express } from "express";
 import { userRouter } from "./user.router";
 import { productRouter } from "./product.router";
 import { cartRouter } from "./cart.router";
-// import { sellRouter } from "./sell.router";
+import { sellRouter } from "./sell.router";
 
 export const allRoutes = (app: Express) => {
   app.use("/user", userRouter());
   app.use("/product", productRouter());
   app.use("/cart", cartRouter());
-  // app.use("/buy", sellRouter());
+  app.use("/buy", sellRouter());
 };
