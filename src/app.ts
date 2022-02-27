c4a02afba223de269cbf988c9e110b98a2bec5ee
André Kuratomi <andrekuratomi@gmail.com>
2022-02-27 20:36:51 -0300

    feat: swagger lib added, imported, configured and first draft of swagger json made

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 import { allRoutes } from "./routes";
 import { handleError } from "./middlewares/errors.middlewares";
@@ -9,6 +11,11 @@ dotenv.config();
 const app: Express = express();
 
 app.use(express.json());
+app.use(
+  "/api-documentation",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
 allRoutes(app);
 app.use(handleError);
 
